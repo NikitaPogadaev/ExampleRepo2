7d0349c9222f0e24732a7727964a28aaf6e6dbee
NikitaPogadaev <pognick@mail.ru>
2025-04-29 10:18:26 +0300

    changing CI

--- tests/FibTests.cpp
+++ tests/FibTests.cpp
@@ -7,7 +7,7 @@
 TEST(Fib, SimpleEqual)
 {
     fibnum::Fib f;
-    int n = 12;
+    int n = 11;
     EXPECT_EQ(n < 30, true);
     std::cout << f.result(n) << '\n';
     EXPECT_EQ(f.result(n), fibfunction(n));
